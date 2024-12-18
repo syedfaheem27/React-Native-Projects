cba6373e515f8cd091f5f1ba88782a4492f8e03e
syedfaheem27 <syedfaheem2719@gmail.com>
2024-12-19 00:00:19 +0530

    Add button styling

--- GuessMyNum/components/PrimaryButton.js
+++ GuessMyNum/components/PrimaryButton.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+  const handlePress = () => console.log("pressed");
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        android_ripple={{ color: "#fde68a" }}
+        style={styles.buttonInnerContainer}
+        onPress={handlePress}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    backgroundColor: "#451a03",
+    margin: 4,
+    borderRadius: 100,
+    elevation: 2,
+    shadowColor: "#451a03",
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
