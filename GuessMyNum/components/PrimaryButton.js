9e524eb46ed85fcf40942c7e2a8c97ce62e2bd6e
syedfaheem27 <syedfaheem2719@gmail.com>
2024-12-19 11:44:03 +0530

    Add handlers to confirm and reset

--- GuessMyNum/components/PrimaryButton.js
+++ GuessMyNum/components/PrimaryButton.js
@@ -1,7 +1,6 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-const PrimaryButton = ({ children }) => {
-  const handlePress = () => console.log("pressed");
+const PrimaryButton = ({ children, onPress }) => {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -11,7 +10,9 @@ const PrimaryButton = ({ children }) => {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={handlePress}
+        onPress={
+          onPress ? onPress : () => console.log("yet not assigned a function")
+        }
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
--- GuessMyNum/screens/StartGame.js
+++ GuessMyNum/screens/StartGame.js
@@ -1,7 +1,28 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 const StartGame = () => {
+  const [enteredText, setEnteredText] = useState("");
+
+  const inputTextHandler = (txt) => {
+    setEnteredText(txt);
+  };
+
+  const confirmTextHandler = () => {
+    const num = parseInt(enteredText, 10);
+    console.log(num);
+    if (isNaN(num) || num <= 0 || num > 99) {
+      Alert.alert("Invalid Number", "The number should be between 1 and 99", [
+        { text: "Okay", style: "cancel", onPress: resetTextHandler },
+      ]);
+      return;
+    }
+
+    console.log(enteredText);
+  };
+  const resetTextHandler = () => setEnteredText("");
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,13 +32,15 @@ const StartGame = () => {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        value={enteredText}
+        onChangeText={inputTextHandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetTextHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmTextHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
