ffeaeafc63cbe278d976a4a75ac9ab6632819376
syedfaheem27 <syedfaheem2719@gmail.com>
2025-01-10 23:46:29 +0530

    Add category ids on meal screen

--- NativeMeals/screens/Category.screen.js
+++ NativeMeals/screens/Category.screen.js
@@ -1,15 +1,22 @@
 import { FlatList, View } from "react-native";
 import { CATEGORIES } from "../data/Categories";
 import CategoryTile from "../components/CategoryTile";
+import { useNavigation } from "@react-navigation/native";
 
-const CategoryScreen = ({ navigation }) => {
-  const pressHanndler = () => {
-    navigation.navigate("MealsOverview");
+// const CategoryScreen = ({ navigation }) => {
+
+const CategoryScreen = () => {
+  const navigation = useNavigation();
+  const pressHanndler = (id) => {
+    // navigation.navigate("MealsOverview");
+    navigation.navigate("MealsOverview", {
+      categoryId: id,
+    });
   };
   const renderCategories = (itemObj) => {
     return (
       <CategoryTile
-        onPress={pressHanndler}
+        onPress={pressHanndler.bind(null, itemObj.item.id)}
         title={itemObj.item.title}
         color={itemObj.item.color}
       />
--- NativeMeals/screens/MealsOvervIewScreen.js
+++ NativeMeals/screens/MealsOvervIewScreen.js
@@ -1,9 +1,14 @@
+import { useRoute } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 
+// const MealsOverviewScreen = ({ route }) => {
+
 const MealsOverviewScreen = () => {
+  const route = useRoute();
+  const { categoryId } = route.params;
   return (
     <View>
-      <Text>Meals Overview</Text>
+      <Text>Meals Overview - {categoryId}</Text>
     </View>
   );
 };
