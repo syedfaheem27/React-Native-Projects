9510ab67234885a2c32c9dacfc572a4c2849df1d
syedfaheem27 <syedfaheem2719@gmail.com>
2025-01-11 00:03:24 +0530

    Render meal item titles

--- NativeMeals/screens/MealsOvervIewScreen.js
+++ NativeMeals/screens/MealsOvervIewScreen.js
@@ -1,14 +1,32 @@
 import { useRoute } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { MEALS } from "../data/Categories";
 
 // const MealsOverviewScreen = ({ route }) => {
 
 const MealsOverviewScreen = () => {
   const route = useRoute();
   const { categoryId } = route.params;
+
+  const selectedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.includes(categoryId) >= 0;
+  });
+  const renderMealItem = (itemObj) => {
+    return (
+      <View>
+        <Text>{itemObj.item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View>
-      <Text>Meals Overview - {categoryId}</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={selectedMeals}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
